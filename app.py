9cd669b60b9e0688ab4389354bce1b89a9c706b9
Mohammad Ali Haider <alihaider86@hotmail.com>
2026-05-08 19:01:24 +0500

    Test CI pipeline

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return f"Hello from {socket.gethostname()}"
+    return f"Hello from {socket.gethostname()} - CI/CD Working!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
